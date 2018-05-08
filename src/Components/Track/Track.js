041839ae6c2c6d1fea43ca887c74368d1cac3f1e
KellyJupe <37583656+KellyJupe@users.noreply.github.com>
2018-05-07 22:22:24 -0400

    5/7 pm

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -6,17 +6,17 @@ class Track extends React.Component {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3></h3>
+          <p></p>
         </div>
-        <a className="Track-action"><!-- + or - will go here --></a>
+        <a className="Track-action"></a>
       </div>
     )
   }
 }
 
-function renderAction() => {
-  if { ('isRemoval === isRemoval ? isRemoval');
+function renderAction() {
+  if ('isRemoval === isRemoval ? isRemoval') {
   return '-';
   }
   else {
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './TrackList.css';
+import '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-          tracks = this.props.searchResults;
+      {
+          tracks = this.props.tracks.map(function(tracks, i) {
+              return <Track key={track.id} tracks={tracks}/>;
+          })
+        }
       </div>
     );
   }
